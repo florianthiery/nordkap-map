d12b48eca70cfdf28cd77a754bfefb4daf50ddb0
florianthiery <web@florian-thiery.de>
2019-07-03 16:50:15 +0200

    Update action.js

--- docs/action.js
+++ docs/action.js
@@ -14,11 +14,21 @@ L.tileLayer(CDB_URL, {
 
 //var photos = [];
 
+var link_d1 = "https://my.hidrive.com/share/###";
+var link_d2 = "https://my.hidrive.com/share/###";
+var link_d3 = "https://my.hidrive.com/share/###";
+var link_d4 = "https://my.hidrive.com/share/###";
+var link_d5 = "https://my.hidrive.com/share/###";
+var link_d6 = "https://my.hidrive.com/share/###";
+var link_d7 = "https://my.hidrive.com/share/###";
+var link_d8 = "https://my.hidrive.com/share/###";
+var link_d9 = "https://my.hidrive.com/share/###";
+
 var photos = [{
         "lat": 63.793483,
         "lng": 20.290592,
         "name": "Tag 1 - Umeå (SWE)- Ankunft",
-        "link": "https://my.hidrive.com/share/###",
+        "link": link_d1,
         "url": "img/d1_1.jpg",
         "thumbnail": "img/d1_1.jpg"
     },
@@ -26,7 +36,7 @@ var photos = [{
         "lat": 64.592043,
         "lng": 18.692441,
         "name": "Tag 1 - Lycksele (SWE) - Hotell Lappland",
-        "link": "https://my.hidrive.com/share/###",
+        "link": link_d1,
         "url": "img/d1_2.jpg",
         "thumbnail": "img/d1_2.jpg"
     },
@@ -34,7 +44,7 @@ var photos = [{
         "lat": 65.051316,
         "lng": 19.410492,
         "name": "Tag 2 - Schweden - Busfahrt",
-        "link": "https://my.hidrive.com/share/###",
+        "link": link_d2,
         "url": "img/d2_1.jpg",
         "thumbnail": "img/d2_1.jpg"
     },
@@ -42,7 +52,7 @@ var photos = [{
         "lat": 65.59444444,
         "lng": 19.17000000,
         "name": "Tag 2 - Arvidsjaur (SWE) - Lappstaden",
-        "link": "https://my.hidrive.com/share/###",
+        "link": link_d2,
         "url": "img/d2_2.jpg",
         "thumbnail": "img/d2_2.jpg"
     },
@@ -50,7 +60,7 @@ var photos = [{
         "lat": 65.59472222,
         "lng": 19.16944444,
         "name": "Tag 2 - Arvidsjaur (SWE) - Kirche",
-        "link": "https://my.hidrive.com/share/###",
+        "link": link_d2,
         "url": "img/d2_3.jpg",
         "thumbnail": "img/d2_3.jpg"
     },
@@ -58,7 +68,7 @@ var photos = [{
         "lat": 65.59361111,
         "lng": 19.17277778,
         "name": "Tag 2 - Arvidsjaur (SWE) - Elch/Stadt",
-        "link": "https://my.hidrive.com/share/###",
+        "link": link_d2,
         "url": "img/d2_4.jpg",
         "thumbnail": "img/d2_4.jpg"
     },
@@ -66,7 +76,7 @@ var photos = [{
         "lat": 66.04555556,
         "lng": 17.87138889,
         "name": "Tag 2 - Arjeplog (SWE)",
-        "link": "https://my.hidrive.com/share/###",
+        "link": link_d2,
         "url": "img/d2_5.jpg",
         "thumbnail": "img/d2_5.jpg"
     },
@@ -74,7 +84,7 @@ var photos = [{
         "lat": 66.50250000,
         "lng": 16.41444444,
         "name": "Tag 2 - Schweden - Busfahrt",
-        "link": "https://my.hidrive.com/share/###",
+        "link": link_d2,
         "url": "img/d2_6.jpg",
         "thumbnail": "img/d2_6.jpg"
     },
@@ -82,7 +92,7 @@ var photos = [{
         "lat": 66.54805556,
         "lng": 16.33444444,
         "name": "Tag 2 - Polarkreis",
-        "link": "https://my.hidrive.com/share/###",
+        "link": link_d2,
         "url": "img/d2_7.jpg",
         "thumbnail": "img/d2_7.jpg"
     },
@@ -90,7 +100,7 @@ var photos = [{
         "lat": 66.67250000,
         "lng": 16.12583333,
         "name": "Tag 2 - Schweden - Busfahrt",
-        "link": "https://my.hidrive.com/share/###",
+        "link": link_d2,
         "url": "img/d2_8.jpg",
         "thumbnail": "img/d2_8.jpg"
     },
@@ -98,7 +108,7 @@ var photos = [{
         "lat": 66.75500000,
         "lng": 15.83472222,
         "name": "Tag 2 - Schweden/Norwegen - Grenze",
-        "link": "https://my.hidrive.com/share/###",
+        "link": link_d2,
         "url": "img/d2_9.jpg",
         "thumbnail": "img/d2_9.jpg"
     },
@@ -106,7 +116,7 @@ var photos = [{
         "lat": 66.81491340,
         "lng": 15.43418190,
         "name": "Tag 2 - Norwegen - Busfahrt",
-        "link": "https://my.hidrive.com/share/###",
+        "link": link_d2,
         "url": "img/d2_10.jpg",
         "thumbnail": "img/d2_10.jpg"
     },
@@ -114,7 +124,7 @@ var photos = [{
         "lat": 67.103660,
         "lng": 15.391583,
         "name": "Tag 2 - Rognan (NOR) - Rognan Hotell",
-        "link": "https://my.hidrive.com/share/###",
+        "link": link_d2,
         "url": "img/d2_11.jpg",
         "thumbnail": "img/d2_11.jpg"
     }
